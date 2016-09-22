779680fd52bfe5e4d2f61c7e6746f9b7fa3613c9
gslAgile <gsosaludu@gmail.com>
2016-09-22 03:21:30 -0300

    Corrijo error de operaciones SRL y SRA y agrego nuevo testbench v3

--- TP1_ALU/ALU_sec.v
+++ TP1_ALU/ALU_sec.v
@@ -20,7 +20,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 module ALU_sec
 #(parameter msb = 7)
-(buf_A, buf_B, buf_Op, p_a, p_b, p_c, buf_R);
+(buf_A, buf_B, buf_Op, p_a, p_b, p_c, clk, buf_R, cout, ovf);
 	
 	// Buffers de entrdas de ALU (switches)
 	input [msb:0] buf_A, buf_B;
@@ -29,16 +29,25 @@ module ALU_sec
 	// Entradas de seleccion de Buffer (pulsadores)
 	input wire p_a, p_b, p_c;
 	
+	// Entrada de clock
+	input clk;
+	
 	// Registros de memoria ALU (registros internos)
 	reg signed [msb:0] dato_A = 0;
 	reg signed [msb:0] dato_B = 0;
 	reg [5:0] dato_Op = 6'b000000;
 	
 	// Salida como registro de la ALU (resultados sobre LEDs)
-	output wire [msb:0] buf_R;
+	output wire signed [msb:0] buf_R;
+	
+	// Salida de carry
+	output wire cout;
+	
+	// Salida de overflow
+	output wire ovf;
 	
 	//
-	always@(p_a or p_b or p_c)
+	always@(posedge clk)//(p_a or p_b or p_c)
 	// Detectamos seleccion de pulsador (forma 2)
 	begin
 		if(p_a == 1'b1)
@@ -55,15 +64,19 @@ module ALU_sec
 //		3'b001: dato_Op = buf_in[5:0];
 //		default: dato_Op = 6'b100100;
 //	endcase
-		
-	
-	assign buf_R = (dato_Op==6'b100000) ? dato_A+dato_B:// ADD
+
+	// Calculo de resultado segun operacion (dato_Op)
+	assign {cout,buf_R} = (dato_Op==6'b100000) ? dato_A+dato_B:// ADD
 						(dato_Op==6'b100010) ? dato_A-dato_B:// SUB
 						(dato_Op==6'b100100) ? dato_A&dato_B:// AND
 						(dato_Op==6'b100101) ? dato_A|dato_B://OR	
 						(dato_Op==6'b100110) ? dato_A^dato_B:// XOR
-						(dato_Op==6'b000011) ? dato_A <<< dato_B://{dato_A[7],dato_A[7:1]}:// SRA
-						(dato_Op==6'b000010) ? dato_A >> dato_B:// SRL
-						(dato_Op==6'b100111) ? ~|dato_A: 0;// NOR
+						(dato_Op==6'b000011) ? {32'hffff,dato_A}>>> 1://{dato_A[7],dato_A[7:1]}:// SRA
+						(dato_Op==6'b000010) ? {0,dato_A} >> dato_B:// SRL
+						(dato_Op==6'b100111) ? ~(dato_A|dato_B): 0;// NOR
+	
+	// Calculo de overflow: en base a ultimo bit msb y carry (cout)
+	assign ovf = (dato_Op==6'b100000) ? buf_R[msb]^cout:// ovf en la suma ADD
+					 (dato_Op==6'b100010) ? buf_R[msb]^cout: 0;// ovf en la resta SUB
 
 endmodule
---
+++ TP1_ALU/test_ALU_sec_v3.v
@@ -0,0 +1,157 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   18:49:16 09/21/2016
+// Design Name:   ALU_sec
+// Module Name:   /home/gabriel/TP1_ALU/test_ALU_sec_v3.v
+// Project Name:  TP1_ALU
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: ALU_sec
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module test_ALU_sec_v3;
+	
+	// Paramatros ALU
+	parameter msb = 7;
+	
+	// Inputs
+	reg [msb:0] buf_A;
+	reg [msb:0] buf_B;
+	reg [5:0] buf_Op;
+	reg p_a;
+	reg p_b;
+	reg p_c;
+	reg clk;
+
+	// Outputs
+	wire [msb:0] buf_R;
+	wire cout;
+	wire ovf;
+
+	// Instantiate the Unit Under Test (UUT)
+	ALU_sec uut (
+		.buf_A(buf_A), 
+		.buf_B(buf_B), 
+		.buf_Op(buf_Op), 
+		.p_a(p_a), 
+		.p_b(p_b), 
+		.p_c(p_c), 
+		.buf_R(buf_R), 
+		.clk(clk),
+		.cout(cout),
+		.ovf(ovf)
+	);
+
+	initial begin
+		// Initialize Inputs
+		buf_A = 20; // Cargamos B
+		buf_B = 0;
+		buf_Op = 6'b100100;
+		p_a = 1; // pulsamos carga de A
+		p_b = 0;
+		p_c = 0;
+
+		// Wait 100 ns for global reset to finish
+		// Add stimulus here
+		#100; // Cargamos A
+		buf_B = 7;
+		p_a = 0;
+		p_b = 1; // pulsamos carga de B
+		p_c = 0;
+		
+		#100; // Cargamos operacion suma
+		buf_Op = 6'b100000; // ADD
+		p_a = 0;
+		p_b = 0;
+		p_c = 1; // pulsamos para cargar operacion
+		
+		#100; // dejamos lapso sin pulsar, para verificar funcionamiento de proxima op
+		buf_Op = 0;
+		p_a = 0;
+		p_b = 0;
+		p_c = 0;
+		
+		#100; // Cargamos operacion resta
+		buf_Op = 6'b100010; // SUB
+		p_a = 0;
+		p_b = 0;
+		p_c = 1; // pulsamos para cargar operacion
+		
+		#100; // Cargamo operacion and
+		buf_Op = 6'b100100;// AND
+		p_a = 0;
+		p_b = 0;
+		p_c = 1; // pulsamos para cargr la operacion
+		
+		#100; // Cargamos operacion or
+		buf_Op = 6'b100101; // OR
+		p_a = 0;
+		p_b = 0;
+		p_c = 1; // pulsamos para cargar operacion
+		
+		#100; // Cargamos operacion xor
+		buf_Op = 6'b100110; // XOR
+		p_a = 0;
+		p_b = 0;
+		p_c = 1; // pulsamos para cargar operacion
+		
+		#100; // dejamos lapso sin pulsar, para verificar funcionamiento de proxima op
+		buf_Op = 0;
+		buf_B = 2;// cargamos op B con 2, para desplazar 2 lugares
+		p_a = 0;
+		p_b = 1; // pulsamos para carga de op B
+		p_c = 0;
+		
+		#100; // dejamos lapso sin pulsar, para verificar funcionamiento de proxima op
+		buf_Op = 6'b000011;
+		buf_A = -12; // cargamos operador A con negativo para verificar sra
+		p_a = 1;// pulsamos para cargar A
+		p_b = 0;
+		p_c = 0;
+		
+		#100; // Cargamos operacion desplazamiento sra
+		buf_Op = 6'b000011; // SRA
+		p_a = 0;
+		p_b = 0;
+		p_c = 1; // pulsamos para cargar operacion
+		
+		#100; // Cargamos operacion desplazamiento srl
+		buf_Op = 6'b000010; // SRL
+		p_a = 0;
+		p_b = 0;
+		p_c = 1; // pulsamos para cargar operacion
+		
+		#100; // Cargamos operacion nor
+		buf_Op = 6'b100111; // NOR
+		p_a = 0;
+		p_b = 0;
+		p_c = 1; // pulsamos para cargar operacion
+
+	end
+	
+	// Generacion de clock en circuito secuencial
+	always begin
+		clk = 1'b0;
+		#(100/2) clk = 1'b1;
+		#(100/2);
+	
+	end
+	
+	// Analisis
+	initial $monitor($time, buf_A, buf_B, buf_Op, buf_R, p_a, p_b, p_c, cout);
+      
+endmodule
+
